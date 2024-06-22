2d56f5a34cff6644672ba700e6d88939e034403f
Kuba Podkomórka <jpodkomorka19@gmail.com>
2024-06-22 23:04:55 +0200

    Added frontend products filtering and fetching  with categories

---
+++ summer-shop/frontend/src/routes/shopping/+page.server.js
@@ -0,0 +1,12 @@
+
+export async function load({ fetch }) {
+    const categoriesResponse = await fetch('http://localhost:8000/hq_products/categories');
+    const categoriesData = await categoriesResponse.json();
+    const productsResponse = await fetch('http://localhost:8000/products_joined');
+    const productsData = await productsResponse.json();
+
+    return {
+        categories: categoriesData !== null ? categoriesData : [],
+        products: productsData !== null ? productsData : []
+    };
+}
